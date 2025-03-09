e898442aac7a75041c869c24e958e2fd2c96e547
Felixnsg <kzam1000@gmail.com>
2025-03-09 15:36:57 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import openai
+import speech_recognition as sr
+import pyttsx3
+import cv2
+import mediapipe as mp
+import numpy as np
+
+print("All libraries are installed correctly!")
